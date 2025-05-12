1afedd119b34f51a27978d5f2734a45dbca0103f
mmhospedagem <contato@mmhospedagem.com.br>
2025-05-12 14:49:35 -0300

    refactor(validate-connection): enhance HMAC and signature

--- src/Utils/validate-connection.ts
+++ src/Utils/validate-connection.ts
@@ -128,16 +128,23 @@ export const configureSuccessfulPairing = (
 
 	const bizName = businessNode?.attrs.name
 	const jid = deviceNode.attrs.jid
+	const isHosted = !!businessNode
 
 	const { details, hmac } = proto.ADVSignedDeviceIdentityHMAC.decode(deviceIdentityNode.content as Buffer)
 	// check HMAC matches
-	const advSign = hmacSign(details, Buffer.from(advSecretKey, 'base64'))
+	const advSign = isHosted
+		? hmacSign(Buffer.concat([Buffer.from([6, 5]), details!]), Buffer.from(advSecretKey, 'base64'))
+		: hmacSign(details!, Buffer.from(advSecretKey, 'base64'))
+	
 	if(Buffer.compare(hmac, advSign) !== 0) {
 		throw new Boom('Invalid account signature')
 	}
 
 	const account = proto.ADVSignedDeviceIdentity.decode(details)
-	const { accountSignatureKey, accountSignature, details: deviceDetails } = account
+	const accountMsg = isHosted
+		? Buffer.concat([Buffer.from([6, 5]), deviceDetails!, signedIdentityKey.public])
+		: Buffer.concat([Buffer.from([6, 0]), deviceDetails!, signedIdentityKey.public])
+
 	// verify the device signature matches
 	const accountMsg = Buffer.concat([ Buffer.from([6, 0]), deviceDetails, signedIdentityKey.public ])
 	if(!Curve.verify(accountSignatureKey, accountMsg, accountSignature)) {
@@ -145,7 +152,9 @@ export const configureSuccessfulPairing = (
 	}
 
 	// sign the details with our identity key
-	const deviceMsg = Buffer.concat([ Buffer.from([6, 1]), deviceDetails, signedIdentityKey.public, accountSignatureKey ])
+	const devicePrefix = isHosted ? Buffer.from([6, 6]) : Buffer.from([6, 1])
+	const deviceMsg = Buffer.concat([devicePrefix, deviceDetails!, signedIdentityKey.public, accountSignatureKey!])
+	
 	account.deviceSignature = Curve.sign(signedIdentityKey.private, deviceMsg)
 
 	const identity = createSignalIdentity(jid, accountSignatureKey)
