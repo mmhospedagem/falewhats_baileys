ddad03c43ab4dee440b607a1efc031bf6eee9729
mmhospedagem <contato@mmhospedagem.com.br>
2025-11-12 17:23:42 -0300

    teste

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -1111,7 +1111,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
                 return [{
                     tag: "native_flow",
                     attrs: {
-                        name: nativeButton.name!
+                        name: nativeButton.name?
                     }
                 }];
             } else {
